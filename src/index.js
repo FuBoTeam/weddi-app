22a4d3e2f4883600fcfed89d70031569ea5c098f
Ben Liu <ben196888@gmail.com>
2018-05-06 23:02:11 +1000

    Rearrange code.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Board from './Board';
+import registerServiceWorker from './registerServiceWorker';
 
 const component = document.location.pathname.toLowerCase().indexOf('board') > -1 ? <Board /> : <App />;
 
