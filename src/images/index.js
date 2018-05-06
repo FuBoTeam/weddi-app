69d37234e918095c0fc1d3031a120cce1e4cf790
Ben Liu <ben196888@gmail.com>
2018-05-06 20:44:15 +1000

    Refactor getImageUrl

--- src/images/index.js
+++ src/images/index.js
@@ -3,10 +3,6 @@ const IMG_ENDPOINT = `https://storage.googleapis.com/wedding_iota/`;
 const IMG_SIZE_S = '_small';
 const IMG_SIZE_M = '';
 
-// Default is small size
-const getFilenameByKey = (key, size = IMG_SIZE_S) => `${key}${size}`;
-
-export const getImageURL = (size = IMG_SIZE_S) => {
-  const filename = getFilenameByKey(0, size);
-  return `${IMG_ENDPOINT}${filename}`;
+export const getImageUrl = (key, size = IMG_SIZE_S) => {
+  return `${IMG_ENDPOINT}${key}${size}.jpg`;
 };
