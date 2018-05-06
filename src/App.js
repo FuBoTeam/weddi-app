1b7aeb6c5b093f4a49b7219a7901f9c519d330e4
Ben Liu <ben196888@gmail.com>
2018-05-06 20:17:05 +1000

    Remove logo and add mock form.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isValidate: false,
+      form: {
+        name: undefined,
+        greetings: undefined,
+        imgUrl: undefined,
+      }
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Greetings</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <label> Name <input type="text" name="name" required /></label><br />
+          <label> Greetings <input type="text" name="greetings" required /></label><br />
+          <div>
+          Picture
+            <label>
+              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
+              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/13_small.jpg" />
+            </label>
+            <label>
+              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
+              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/20_small.jpg" />
+            </label>
+            <label>
+              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
+              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/50_small.jpg" />
+            </label>
+            <label>
+              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
+              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/80_small.jpg" />
+            </label>
+            <label>
+              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
+              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/0_small.jpg" />
+            </label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
