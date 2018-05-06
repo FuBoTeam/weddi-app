7a782a5f7aef3b2fe39786b7279ce6181038e241
Ben Liu <ben196888@gmail.com>
2018-05-06 21:00:46 +1000

    Introduce getImageUrl function. Replace mock datas by function call.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { getImageUrl } from './images';
+
 class App extends Component {
   constructor() {
     super();
@@ -14,6 +16,13 @@ class App extends Component {
     };
   }
   render() {
+    const images = [getImageUrl(13), getImageUrl(20), getImageUrl(50), getImageUrl(80), getImageUrl(1)];
+    const picRadios = images.map((url, i) => (
+      <label key={i}>
+        <input type="radio" name="imgUrl" value={url} required />
+        <img style={{height: '150px'}} src={url} alt={url} />
+      </label>
+    ));
     return (
       <div className="App">
         <header className="App-header">
@@ -22,29 +31,7 @@ class App extends Component {
         <form>
           <label> Name <input type="text" name="name" required /></label><br />
           <label> Greetings <input type="text" name="greetings" required /></label><br />
-          <div>
-          Picture
-            <label>
-              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
-              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/13_small.jpg" />
-            </label>
-            <label>
-              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
-              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/20_small.jpg" />
-            </label>
-            <label>
-              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
-              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/50_small.jpg" />
-            </label>
-            <label>
-              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
-              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/80_small.jpg" />
-            </label>
-            <label>
-              <input type="radio" name="imgUrl" value="https://storage.googleapis.com/wedding_iota/13_small.jpg" required />
-              <img style={{height: '150px'}} src="https://storage.googleapis.com/wedding_iota/0_small.jpg" />
-            </label>
-          </div>
+          <div> Picture {picRadios}</div>
           <button type="submit">Submit</button>
         </form>
       </div>
