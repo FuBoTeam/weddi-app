d35f84f79da78bcd72b579eeaccf411457355989
Ben Liu <ben196888@gmail.com>
2018-05-08 22:50:27 +1000

    Update form and app.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Board from './Board';
+import Api from './api';
 import registerServiceWorker from './registerServiceWorker';
+
+Api.init();
 
 const component = document.location.pathname.toLowerCase().indexOf('dashboard') > -1 ? <Board /> : <App />;
 
