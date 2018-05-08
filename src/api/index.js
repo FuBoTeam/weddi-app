ca8b4710626b6f06dd546fabbf83d8d3b2e2d6d8
Ben Liu <ben196888@gmail.com>
2018-05-08 23:30:12 +1000

    Add getPost in api.

--- src/api/index.js
+++ src/api/index.js
@@ -31,7 +31,6 @@ const init = () => {
 
 const writePost = (postData) => {
   const postId = api.getDB().ref().child('posts').push().key;
-  console.log(postId, typeof postId);
   const wrappedPostData = {
     ...postData,
     modifiedTime: new Date().toISOString(),
@@ -40,7 +39,15 @@ const writePost = (postData) => {
   return api.getDB().ref(`posts/${postId}`).set(wrappedPostData);
 };
 
+const getPost = (callback) => {
+  const postRef = api.getDB().ref('posts').limitToLast(100);
+  postRef.on('child_added', (data) => {
+    callback(data.val());
+  });
+};
+
 export default {
   init,
   writePost,
+  getPost,
 };
