84574d41411cbb8c744aafa9a62215825887a6fd
Ben Liu <ben196888@gmail.com>
2018-05-09 23:34:32 +1000

    Update dialog style for reative.

--- src/Board/Dialog.js
+++ src/Board/Dialog.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import './board.css';
 
 export default class Dialog extends Component {
+  constructor() {
+    super();
+    this.img =  new Image();
+    const t = this;
+    this.img.onload = function() {
+      t.setState(() => ({
+        imgWidth: this.width || 0,
+        imgHeight: this.height || 0,
+      }));
+    };
+    this.state = {
+      imgWidth: 0,
+      imgHeight: 0,
+    };
+  }
+
+  componentWillReceiveProps({ user, show }) {
+    this.img.src = show ? user.imgUrl : '';
+  }
 
   render() {
     const { user, show } = this.props;
 
+    const mayMobile = window.innerWidth < 1000;
+    const verticalImg = this.state.imgWidth < this.state.imgHeight;
+    const modalStyle = (show && mayMobile && verticalImg) ? { margin: '10% 20% 50px' } : {};
+
     return (
       <div className={show ? 'dialog show-dialog' : 'dialog'}>
-        <div className="modal-sm">
+        <div className="modal-sm" style={modalStyle}>
           <div className="image-container">
             <img className="user-image" src={user.imgUrl} alt="images" />
           </div>
