6691c5a1d77c9821f71a53411eb0e65b96415fc7
Ben Liu <ben196888@gmail.com>
2019-10-19 20:23:24 +0700

    Bugfix: -1.jpg

--- src/utils/random.js
+++ src/utils/random.js
@@ -3,7 +3,7 @@ import range from 'lodash/range';
 
 export const combination = (n, k) => {
   return range(n - k, n).reduce((ans, j) => {
-    const t = random(j - 1);
+    const t = random(j);
     if (ans.includes(t)) {
       return [...ans, j];
     }
