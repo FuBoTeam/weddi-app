2abc706c261b0c3b0bea0da224f34b5d4804e7f6
Ben Liu <ben196888@gmail.com>
2019-11-07 22:48:01 +0700

    Updates

---
+++ precache-manifest.074724e1cc4c23f4472aa70d0a6a3f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd17c2f997f53a6a252ad125fb5484e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cfde2e60bf18ce19a03",
+    "url": "/static/css/main.2c66c6c7.chunk.css"
+  },
+  {
+    "revision": "a85c1a75911ca0bf8da2",
+    "url": "/static/js/2.118d682a.chunk.js"
+  },
+  {
+    "revision": "7cfde2e60bf18ce19a03",
+    "url": "/static/js/main.c0a94011.chunk.js"
+  },
+  {
+    "revision": "cd289aaa0d3823bae688",
+    "url": "/static/js/runtime-main.ea4116d0.js"
+  },
+  {
+    "revision": "822699a4761030ce91861fe01697129c",
+    "url": "/static/media/loading.822699a4.gif"
+  }
+]);
