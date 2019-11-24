1f5d2f3f8b4ff1fe83dd88147af4e3863a4be89d
Ben Liu <ben196888@gmail.com>
2019-11-24 10:06:08 +0700

    Update API files to be in typescript

---
+++ src/api/index.ts
@@ -0,0 +1,68 @@
+import * as firebase from "firebase/app";
+import "firebase/database";
+import "firebase/storage";
+
+import Configs from "../configs";
+
+class apiBase {
+  app: firebase.app.App;
+  database: firebase.database.Database;
+  storage: firebase.storage.Storage;
+  constructor() {
+    this.app = firebase.initializeApp(Configs.firebase);
+    this.database = this.app.database();
+    this.storage = this.app.storage();
+  }
+}
+
+let api: apiBase;
+
+const init = () => {
+  if (!api) {
+    api = new apiBase();
+  }
+  return api;
+};
+
+const writePost = (postData: object) => {
+  const postId = api.database.ref("posts").push().key;
+  if (!postId) {
+    throw new Error("post id is empty");
+  }
+  const wrappedPostData = {
+    ...postData,
+    modifiedTime: new Date().toISOString(),
+    userAgent: navigator.userAgent,
+    id: postId
+  };
+  return api.database
+    .ref("posts")
+    .child(postId)
+    .set(wrappedPostData);
+};
+
+const getPost = (callback: Function) => {
+  const postRef = api.database.ref("posts");
+  postRef.on("child_added", (data: firebase.database.DataSnapshot) => {
+    callback(data.val());
+  });
+};
+
+const listAllImages = () => {
+  return api.storage.ref(Configs.img.namespace).listAll();
+};
+
+const uploadImage = (imgName: string, image: Blob) => {
+  return api.storage
+    .ref(Configs.img.namespace)
+    .child(imgName)
+    .put(image);
+};
+
+export default {
+  init,
+  writePost,
+  getPost,
+  listAllImages,
+  uploadImage
+};
