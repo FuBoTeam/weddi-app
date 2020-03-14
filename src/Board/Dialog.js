db090f6d7c85478244ffeaed133e0e806e84fa86
Ben Liu <ben196888@gmail.com>
2020-03-14 18:26:39 +0700

    Refactor: migrate dialog from full component to hooks

--- src/Board/Dialog.js
+++ src/Board/Dialog.js
@@ -1,50 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './board.scss';
 
-export default class Dialog extends Component {
-  constructor() {
-    super();
-    this.img =  new Image();
-    const t = this;
-    this.img.onload = function() {
-      t.setState(() => ({
+const Dialog = (props) => {
+  const [state, setState] = useState({ imgWidth: 0, imgHeight: 0 });
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = function() {
+      setState(() => ({
         imgWidth: this.width || 0,
         imgHeight: this.height || 0,
       }));
     };
-    this.state = {
-      imgWidth: 0,
-      imgHeight: 0,
+    img.src = props.show ? props.user.imgUrl : '';
+    return () => {
+      img.src = '';
     };
-  }
+  }, [props.show, props.user.imgUrl]);
 
-  componentDidMount() {
-    this.img.src = this.props.show ? this.props.user.imgUrl : '';
-  }
+  const { user, show } = props;
 
-  componentDidUpdate() {
-    this.img.src = this.props.show ? this.props.user.imgUrl : '';
-  }
+  const mayMobile = window.innerWidth < 1000;
+  const verticalImg = state.imgWidth < state.imgHeight;
+  const modalStyle = (show && mayMobile && verticalImg) ? { margin: '10% 20% 50px' } : {};
 
-  render() {
-    const { user, show } = this.props;
-
-    const mayMobile = window.innerWidth < 1000;
-    const verticalImg = this.state.imgWidth < this.state.imgHeight;
-    const modalStyle = (show && mayMobile && verticalImg) ? { margin: '10% 20% 50px' } : {};
-
-    return (
-      <div className={show ? 'dialog show-dialog' : 'dialog'}>
-        <div className="modal-sm" style={modalStyle}>
-          <div className="image-container">
-            <img className="user-image" src={user.imgUrl} alt="images" />
-          </div>
-          <div className="message">
-            <h2>@ {user.name}</h2>
-            <p>{user.greetings}</p>
-          </div>
+  return (
+    <div className={show ? 'dialog show-dialog' : 'dialog'}>
+      <div className="modal-sm" style={modalStyle}>
+        <div className="image-container">
+          <img className="user-image" src={user.imgUrl} alt="images" />
+        </div>
+        <div className="message">
+          <h2>@ {user.name}</h2>
+          <p>{user.greetings}</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Dialog;
