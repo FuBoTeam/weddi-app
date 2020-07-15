3835fffcbfaf27a3f8f5e60123721ade57c90b16
Ben Liu <ben196888@gmail.com>
2020-07-16 01:26:21 +0700

    Refactor: migrate Board to react hook

--- src/Board/index.js
+++ src/Board/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import range from 'lodash/range';
 import './board.scss';
 import loadingIcon from '../images/loading.gif';
@@ -12,102 +12,93 @@ import { preloadImage, getImageUrl } from '../images';
 
 import * as Api from '../api';
 
-export default class Board extends Component {
-  allImgUrls = range(Config.img.totalImgs).map(k => getImageUrl(k));
-  bgImgUrls = combinationList(this.allImgUrls, Config.img.bgImgsShouldBePicked);
-  intervals = [];
-  state = {
-    isLoading: true,
-    modalDisplay: false,
-    user: {},
-    permutation: permutationList(range(this.bgImgUrls.length)),
-    isBgSwitching: false,
-  };
+const Board = (props) => {
+  const allImgUrls = range(Config.img.totalImgs).map(k => getImageUrl(k));
+  const bgImgUrls = combinationList(allImgUrls, Config.img.bgImgsShouldBePicked);
+  const intervals = [];
+  const [isLoading, setIsLoading] = useState(true);
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const [user, setUser] = useState({});
+  const [permutation, setPermutation] = useState(permutationList(range(bgImgUrls.length)));
+  const [isBgSwitching, setIsBgSwitching] = useState(false);
 
-  componentDidMount() {
-    const newFeeds = new Queue();
-    const oldFeeds = new Queue();
+  const newFeeds = new Queue();
+  const oldFeeds = new Queue();
 
-    Api.onNewPost((feed) => newFeeds.enQueue(feed));
-
-    window.onload = () => {
-      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      this.intervals.push(setInterval(() => {
-        this.pickUpFeed(newFeeds, oldFeeds);
-      }, 8000));
-    };
-
-    // Auto refresh background images
-    this.intervals.push(
-      setInterval(() => {
-        this.setState({
-          isBgSwitching: true,
-        });
-        setTimeout(() => {
-          this.setState({
-            permutation: permutationList(range(this.bgImgUrls.length)),
-          });
-          setTimeout(() => {
-            this.setState({
-              isBgSwitching: false,
-            });
-          }, 1000);
-        }, 1000);
-      }, 5 * 60 * 1000)
-    );
-  }
-
-  componentWillUnmount() {
-    this.intervals.forEach(clearInterval);
-  }
-
-  showDialog = (user) => {
-    this.setState({ user: Object.assign({}, user) });
+  const showDialog = (user) => {
+    setUser(user);
     const displayAndHide = () => {
-      this.setState(() => ({ modalDisplay: true }));
+      setModalDisplay(true);
 
       setTimeout(() => {
-        this.setState(() => ({ modalDisplay: false }));
+        setModalDisplay(false);
       }, 5000);
     };
     // preload dialog img and display in dialog
     preloadImage(user.imgUrl, displayAndHide);
-  }
+  };
 
-  pickUpFeed = (newFeeds, oldFeeds) => {
+  const pickUpFeed = (newFeeds, oldFeeds) => {
     const nextFeed = newFeeds.isEmpty() ? oldFeeds.deQueue() : newFeeds.deQueue();
-    this.showDialog(nextFeed);
+    showDialog(nextFeed);
     oldFeeds.enQueue(nextFeed);
-  }
+  };
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="container">
-          {
-            this.state.isLoading && <div className="loading">
-              <img src={loadingIcon} alt="" />
-              <span>Loading...</span>
-            </div>
-          }
-          {
-            this.bgImgUrls.map((bgImgUrl, idx) =>
-              <img
-                key={bgImgUrl}
-                className={
-                  this.state.isLoading || this.state.isBgSwitching ? 'hidden' : 'visible'
-                }
-                style={{order: this.state.permutation[idx]}}
-                src={bgImgUrl}
-                alt=""
-              />
-            )
-          }
-        </div>
-        <Dialog user={this.state.user} show={this.state.modalDisplay} />
-        <a className="message-link" href={`${this.props.match.url}/greetings`}>&lt;&lt; 留下你的祝福</a>
-      </React.Fragment>
+  useEffect(() => {
+    Api.onNewPost((feed) => newFeeds.enQueue(feed));
+
+    window.onload = () => {
+      setIsLoading(isLoading => !isLoading);
+
+      intervals.push(setInterval(() => {
+        pickUpFeed(newFeeds, oldFeeds);
+      }, 8000));
+    };
+
+    // Auto refresh background images
+    intervals.push(
+      setInterval(() => {
+        setIsBgSwitching(true);
+        setTimeout(() => {
+          setPermutation(permutationList(range(bgImgUrls.length)));
+          setTimeout(() => {
+            setIsBgSwitching(false);
+          }, 1000);
+        }, 1000);
+      }, 5 * 60 * 1000)
     );
-  }
-}
+    return () => {
+      intervals.forEach(clearInterval);
+    };
+  });
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        {
+          isLoading && <div className="loading">
+            <img src={loadingIcon} alt="" />
+            <span>Loading...</span>
+          </div>
+        }
+        {
+          bgImgUrls.map((bgImgUrl, idx) =>
+            <img
+              key={bgImgUrl}
+              className={
+                isLoading || isBgSwitching ? 'hidden' : 'visible'
+              }
+              style={{order: permutation[idx]}}
+              src={bgImgUrl}
+              alt=""
+            />
+          )
+        }
+      </div>
+      <Dialog user={user} show={modalDisplay} />
+      <a className="message-link" href={`${props.match.url}/greetings`}>&lt;&lt; 留下你的祝福</a>
+    </React.Fragment>
+  );
+};
+
+export default Board;
