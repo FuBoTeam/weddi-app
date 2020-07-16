1b532ca23be5761280f696ce549abc89cbb0f091
Ben Liu <ben196888@gmail.com>
2020-07-16 21:32:24 +0700

    Refactor: migrate greeting to hook

--- src/Greeting/index.js
+++ src/Greeting/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import range from 'lodash/range';
 import uuid from 'uuid';
 import loadImage from 'blueimp-load-image';
@@ -11,117 +11,107 @@ import Dialog from '../Board/Dialog';
 import loadingIcon from '../images/uploadLoading.svg';
 import * as Api from '../api';
 
-class Greeting extends Component {
-  allImgUrls = range(Config.img.totalImgs).map(k => getImageUrl(k));
-  fmImgsShouldBePicked = Config.img.fmImgsShouldBePicked;
-  imgUrls = combinationList(this.allImgUrls, this.fmImgsShouldBePicked);
-  state = {
-    form: {
-      name: '',
-      greetings: '',
-      pickedImg: {
-        idx: 0,
-        url: this.imgUrls[0],
-      },
-      upload: null,
+const Greeting = (props) => {
+  const allImgUrls = range(Config.img.totalImgs).map(k => getImageUrl(k));
+  const fmImgsShouldBePicked = Config.img.fmImgsShouldBePicked;
+  const imgUrls = combinationList(allImgUrls, fmImgsShouldBePicked);
+  const [form, setForm] = useState({
+    name: '',
+    greetings: '',
+    pickedImg: {
+      idx: 0,
+      url: imgUrls[0],
     },
-    isUploadPage: false,
-    modalDisplay: false,
-    isLoading: false
-  };
+    upload: null,
+  });
+  const [isUploadPage, setIsUploadPage] = useState(false);
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  onFileChangeHandler = this.onFileChangeHandler.bind(this);
-  onTextChangeHandler = this.onTextChangeHandler.bind(this);
-  onSubmitHandler = this.onSubmitHandler.bind(this);
-  isValid = this.isValid.bind(this);
-  getImg = this.getImg.bind(this);
-  plusImgIdx = this.plusImgIdx.bind(this);
-
-  getUpperUrl() {
-    const matchUrl = this.props.match.url;
+  const getUpperUrl = () => {
+    const matchUrl = props.match.url;
     const paths = matchUrl.split('/');
     paths.pop();
     return paths.join('/');
-  }
+  };
 
-  getFormData() {
+  const getFormData = () => {
     return {
-      ...this.state.form,
-      imgUrl: this.state.form.pickedImg.url,
+      ...form,
+      imgUrl: form.pickedImg.url,
     };
-  }
+  };
 
-  onFileChangeHandler(event) {
+  const onFileChangeHandler = (event) => {
     const key = event.target.name;
     const value = event.target.files[0];
     if (key && value && value.type.startsWith('image/')) {
       loadImage(
         value,
         canvas => canvas.toBlob(blob => {
-          this.setState(({ form }) => ({ form: { ...form, [key]: blob } }));
+          setForm(form => ({ ...form, [key]: blob }));
         }, "image/jpeg", 0.75),
         { maxWidth: 2048, maxHeight: 2048, orientation: true, canvas: true, noRevoke: true }
       );
     }
-  }
+  };
 
-  onTextChangeHandler(event) {
+  const onTextChangeHandler = (event) => {
     const key = event.target.name;
     const value = event.target.value.trim();
-    this.setState(({ form }) => ({ form: { ...form, [key]: value } }));
-  }
+    setForm((form) => ({ ...form, [key]: value }));
+  };
 
-  onSubmitHandler(event) {
+  const onSubmitHandler = (event) => {
     event.preventDefault();
-    if (this.isValid()) {
-      return this.uploadFlow();
+    if (isValid()) {
+      return uploadFlow();
     }
-  }
+  };
 
-  async uploadFlow() {
-    this.setState({ isLoading: true });
-    let imgUrl = this.state.form.pickedImg.url;
-    if (this.state.isUploadPage && this.state.form.upload) {
+  const uploadFlow = async () => {
+    setIsLoading(true);
+    let imgUrl = form.pickedImg.url;
+    if (isUploadPage && form.upload) {
       const imgName = uuid.v4();
-      const uploadProc = await Api.uploadImage(imgName, this.state.form.upload);
+      const uploadProc = await Api.uploadImage(imgName, form.upload);
       imgUrl = await uploadProc.ref.getDownloadURL();
-      this.setState({
-        form: {
-          ...this.state.form,
-          pickedImg: {
-            idx: -1,
-            url: imgUrl
-          }
+      setForm(form => ({
+        ...form,
+        pickedImg: {
+          idx: -1,
+          url: imgUrl
         }
-      });
+      }));
     }
-    await Api.writePost(this.getFormData());
+    await Api.writePost(getFormData());
     const updateStateAndRedirect = () => {
-      this.setState({ modalDisplay: true, isLoading: false });
-      setTimeout(() => { this.props.history.push(this.getUpperUrl()); }, 5000);
+      setModalDisplay(true);
+      setIsLoading(false);
+      setTimeout(() => { props.history.push(getUpperUrl()); }, 5000);
     };
     preloadImage(imgUrl, updateStateAndRedirect);
-  }
+  };
 
-  getImg(idx) {
-    return ({ idx, url: this.imgUrls[idx] });
-  }
+  const getImg = (idx) => {
+    return ({ idx, url: imgUrls[idx] });
+  };
 
-  plusImgIdx(i) {
-    const nextIdx = (this.state.form.pickedImg.idx + i + this.fmImgsShouldBePicked) % this.fmImgsShouldBePicked;
+  const plusImgIdx = (i) => {
+    const nextIdx = (form.pickedImg.idx + i + fmImgsShouldBePicked) % fmImgsShouldBePicked;
     // set img idx to render the seleceted img
-    this.setState(({ form }) => ({
-      form: { ...form, pickedImg: this.getImg(nextIdx) }
+    setForm(form => ({
+      ...form, pickedImg: getImg(nextIdx)
     }));
-  }
+  };
 
-  isValid() {
-    const { name, greetings, pickedImg } = this.state.form;
+  const isValid = () => {
+    const { name, greetings, pickedImg } = form;
     return name.trim() !== '' && greetings.trim() !== '' && pickedImg.url !== undefined;
-  }
+  };
 
-  renderPhotoRadios = () => this.imgUrls.map((url, i) => {
-    const checked = this.state.form.pickedImg.idx === i && this.state.form.pickedImg.url === url;
+  const renderPhotoRadios = () => imgUrls.map((url, i) => {
+    const checked = form.pickedImg.idx === i && form.pickedImg.url === url;
     return (
       <React.Fragment key={`image_${i}`}>
         <input hidden type="radio" name="imgUrl" value={url} checked={checked} readOnly />
@@ -131,7 +121,7 @@ class Greeting extends Component {
     );
   });
 
-  renderUploadImageSection() {
+  const renderUploadImageSection = () => {
     return (
       <label className="img-window upload">
         <input
@@ -140,78 +130,76 @@ class Greeting extends Component {
           name="upload"
           placeholder="上傳照片"
           accept="image/*"
-          onChange={this.onFileChangeHandler}
-          disabled={this.state.isLoading}
+          onChange={onFileChangeHandler}
+          disabled={isLoading}
         />
-        {this.state.form.upload && <img src={URL.createObjectURL(this.state.form.upload)} alt="upload preview" />}
-        {!this.state.form.upload && <span className="upload-field">請上傳圖片</span>}
+        {form.upload && <img src={URL.createObjectURL(form.upload)} alt="upload preview" />}
+        {!form.upload && <span className="upload-field">請上傳圖片</span>}
       </label>
     );
-  }
+  };
 
-  renderPickImageSection() {
+  const renderPickImageSection = () => {
     return (
       <React.Fragment>
         <div className="img-window">
-          <div className="numbertext">{this.state.form.pickedImg.idx + 1} / {this.fmImgsShouldBePicked}</div>
-          {this.renderPhotoRadios()}
+          <div className="numbertext">{form.pickedImg.idx + 1} / {fmImgsShouldBePicked}</div>
+          {renderPhotoRadios()}
         </div>
-        <span className="prev" onClick={() => !this.state.isLoading && this.plusImgIdx(-1)}>&#10094;</span>
-        <span className="next" onClick={() => !this.state.isLoading && this.plusImgIdx(1)}>&#10095;</span>
+        <span className="prev" onClick={() => !isLoading && plusImgIdx(-1)}>&#10094;</span>
+        <span className="next" onClick={() => !isLoading && plusImgIdx(1)}>&#10095;</span>
       </React.Fragment>
     );
-  }
+  };
 
-  renderGreetingForm() {
+  const renderGreetingForm = () => {
     return (
-      <form className="greeting-form" onSubmit={this.onSubmitHandler}>
+      <form className="greeting-form" onSubmit={onSubmitHandler}>
         <ul className="tabs-view">
           <li
-            className={ this.state.isUploadPage ? 'pick' : 'pick active' }
-            onClick={() => !this.state.isLoading && this.setState({ isUploadPage: false })}
+            className={ isUploadPage ? 'pick' : 'pick active' }
+            onClick={() => !isLoading && setIsUploadPage(false)}
           >
             挑一張照片
           </li>
           <li
-            className={ this.state.isUploadPage ? 'pick active' : 'pick' }
-            onClick={() => !this.state.isLoading && this.setState({ isUploadPage: true })}
+            className={ isUploadPage ? 'pick active' : 'pick' }
+            onClick={() => !isLoading && setIsUploadPage(true)}
           >
             上傳一張照片
           </li>
         </ul>
         <div className="slideshow-container">
-          <div hidden={!this.state.isUploadPage}>{this.renderUploadImageSection()}</div>
-          <div hidden={this.state.isUploadPage}>{this.renderPickImageSection()}</div>
+          <div hidden={!isUploadPage}>{renderUploadImageSection()}</div>
+          <div hidden={isUploadPage}>{renderPickImageSection()}</div>
         </div>
         <div className="greeting-message-block">
           <label className="input">
             <h2>@</h2>
-            <input type="text" name="name" placeholder="姓名" onChange={this.onTextChangeHandler} required disabled={this.state.isLoading} />
+            <input type="text" name="name" placeholder="姓名" onChange={onTextChangeHandler} required disabled={isLoading} />
           </label>
           <label className="input">
-            <textarea name="greetings" placeholder="祝賀詞" onChange={this.onTextChangeHandler} required disabled={this.state.isLoading} />
+            <textarea name="greetings" placeholder="祝賀詞" onChange={onTextChangeHandler} required disabled={isLoading} />
           </label>
         </div>
-        <button className="btn" type="submit" disabled={this.state.isLoading}>
-          {this.state.isLoading && <img className="loading-img" src={loadingIcon} alt="" />}
+        <button className="btn" type="submit" disabled={isLoading}>
+          {isLoading && <img className="loading-img" src={loadingIcon} alt="" />}
           留言
         </button>
-        <a className="link orange-font" href={this.getUpperUrl()}>去照片牆瞧瞧</a>
+        <a className="link orange-font" href={getUpperUrl()}>去照片牆瞧瞧</a>
       </form>
     );
-  }
+  };
 
-  render() {
-    return (
-      <div className="greeting">
-        <header className="greeting-header">
-          <h1 className="greeting-title orange-font">祝福留言版</h1>
-        </header>
-        {!this.state.modalDisplay && this.renderGreetingForm()}
-        <Dialog user={this.getFormData()} show={this.state.modalDisplay} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="greeting">
+      <header className="greeting-header">
+        <h1 className="greeting-title orange-font">祝福留言版</h1>
+      </header>
+      {!modalDisplay && renderGreetingForm()}
+      <Dialog user={getFormData()} show={modalDisplay} />
+    </div>
+  );
+};
 
 export default Greeting;
