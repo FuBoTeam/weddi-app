14e293414ddd2a0c2f2a2a2b528ffbc302780415
Ben Liu <ben196888@gmail.com>
2021-03-01 00:00:07 +0700

    refactor(random): migrate file from js to ts

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,11 +1,11 @@
-const random = (a = 1, b = 0) => {
+const random = (a = 1, b = 0): number => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const combinationList = (list, k) => {
-  const result = [];
+export const combinationList = <T>(list: T[] , k: number): T[] => {
+  const result: T[] = [];
   /* select k elements n-k <= i < n */
   for (let i = list.length - k; i < list.length; i++) {
     /* A random integer such that 0 <= j < i */
@@ -21,7 +21,7 @@ export const combinationList = (list, k) => {
   return result;
 };
 
-export const permutationList = list => {
+export const permutationList = <T>(list: T[]): T[] => {
   for (let i = 0; i <= list.length - 2; i++) {
     /* A random integer such that i <= j < n */
     const j = random(i, list.length - 1);
