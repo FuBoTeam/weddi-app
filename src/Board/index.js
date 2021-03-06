922ffeb4e8a3843339b6ab8988e22c19e96f54c3
Ben Liu <ben196888@gmail.com>
2021-03-06 23:41:55 +0700

    refactor(Board): background images state be visible, fade-out, hidden, fade-in

--- src/Board/index.js
+++ src/Board/index.js
@@ -9,15 +9,24 @@ import configService from '../services/configService';
 import { combinationList, permutationList } from '../utils/random';
 import { getImageUrl } from '../images';
 
-
 import * as Api from '../api';
+
+const TIME_FADE_OUT = 1000;
+const TIME_FADE_IN = 1000;
+const TIME_HIDDEN = 1500;
+const TIME_UPDATE_BACKGROUND = 30 * 60 * 1000;
+
+const BG_FADE_IN = 'BG_FADE_IN';
+const BG_FADE_OUT = 'BG_FADE_OUT';
+const BG_HIDDEN = 'BG_HIDDEN';
+const BG_VISIBLE = 'BG_VISIBLE';
 
 const Board = (props) => {
   // get all image urls from api
   const allImgUrls = range(configService.config.img.totalImgs).map(k => getImageUrl(k));
   const bgImgUrls = combinationList(allImgUrls, configService.config.img.bgImgsShouldBePicked);
   const [isLoading, setIsLoading] = useState(true);
-  const [isBgSwitching, setIsBgSwitching] = useState(false);
+  const [bgState, setBgState] = useState(BG_HIDDEN);
   const [permutation, setPermutation] = useState(permutationList(bgImgUrls));
 
   useEffect(() => {
@@ -28,20 +37,25 @@ const Board = (props) => {
   });
 
   useEffect(() => {
-    // auto permutate background images
-    const interval = setInterval(() => {
-      setIsBgSwitching(true);
+    if (bgState === BG_FADE_OUT) {
       setTimeout(() => {
+        setBgState(BG_HIDDEN);
         setPermutation((permutation) => permutationList(permutation));
-        setTimeout(() => {
-          setIsBgSwitching(false);
-        }, 1000);
-      }, 1000);
-    }, 5 * 60 * 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  });
+      }, TIME_FADE_OUT);
+    } else if (bgState === BG_HIDDEN) {
+      setTimeout(() => {
+        setBgState(BG_FADE_IN);
+      }, TIME_HIDDEN);
+    } else if (bgState === BG_FADE_IN) {
+      setTimeout(() => {
+        setBgState(BG_VISIBLE);
+      }, TIME_FADE_IN);
+    } else if (bgState === BG_VISIBLE) {
+      setTimeout(() => {
+        setBgState(BG_FADE_OUT);
+      }, TIME_UPDATE_BACKGROUND);
+    }
+  }, [bgState]);
 
   const [modalDisplay, setModalDisplay] = useState(false);
   const newFeeds = useMemo(() => new Queue(), []);
@@ -98,7 +112,11 @@ const Board = (props) => {
             <img
               key={bgImgUrl}
               className={
-                isLoading || isBgSwitching ? 'hidden' : 'visible'
+                isLoading || bgState === BG_HIDDEN ? 'hidden' :
+                  bgState === BG_VISIBLE ? 'visible' :
+                    bgState === BG_FADE_IN ? 'fade-in' :
+                      bgState === BG_FADE_OUT ? 'fade-out' :
+                        ''
               }
               src={bgImgUrl}
               alt=""
