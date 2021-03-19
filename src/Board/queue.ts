8ce9dffe05a7a9690fcee841b4a9bdcc03c1e21c
Ben Liu <ben196888@gmail.com>
2021-03-19 22:59:55 +0700

    refactor(queue): from js to ts

---
+++ src/Board/queue.ts
@@ -0,0 +1,25 @@
+export interface QueueInterface<T> {
+  push: (element: T) => void;
+  pop: () => T;
+  isEmpty: () => boolean;
+}
+
+export default class Queue<T = any> implements QueueInterface<T> {
+  private q: T[];
+  public constructor() {
+    this.q = [];
+  }
+
+  public push(element: T): void {
+    this.q.push(element);
+  }
+
+  public pop(): T {
+    const result = this.q.splice(0, 1)[0];
+    return result;
+  }
+
+  public isEmpty(): boolean {
+    return this.q.length === 0;
+  }
+}
