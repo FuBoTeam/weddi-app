12c73a2936f8c4cd2025cf5dac150fbc61e899da
Ben Liu <ben196888@gmail.com>
2022-02-15 01:24:22 +0700

    refactor(weddi.app): restyle dialog

--- src/Board/Dialog.js
+++ src/Board/Dialog.js
@@ -2,34 +2,30 @@ import React, { useState, useEffect, useRef } from 'react';
 import './board.scss';
 
 const Dialog = ({ user, show }) => {
-  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   const imgRef = useRef();
 
   useEffect(() => {
     // while image url changes set to loading
     if (user.imgUrl) {
-      setLoading(true);
+      setLoaded(false);
     }
   }, [user.imgUrl]);
 
   const onLoad = () => {
-    setLoading(false);
+    setLoaded(true);
   };
 
-  const mayMobile = window.innerWidth < 1000;
-  const verticalImg = !loading && imgRef.current && imgRef.current.clientWidth < imgRef.current.clientHeight;
-  const modalStyle = (!loading && mayMobile && verticalImg) ? { margin: '10% 20% 50px' } : {};
+  const portraitImage = loaded && imgRef.current && imgRef.current.clientWidth < imgRef.current.clientHeight;
 
   return (
-    <div className={`dialog ${show && !loading ? "show-dialog" : ""}`}>
-      <div className="modal-sm" style={modalStyle}>
-        <div className="image-container">
-          <img ref={imgRef} className="user-image" src={user.imgUrl} alt="images" onLoad={onLoad} />
-        </div>
-        <div className="message">
-          <h2>From {user.name}:</h2>
-          <p>{user.greetings}</p>
-        </div>
+    <div className={`dialog ${(show && loaded) ? 'show-dialog' : ''}`}>
+      <div className={`image-container ${portraitImage ? 'portrait' : ''}`}>
+        <img ref={imgRef} className="user-image" src={user.imgUrl} alt="images" onLoad={onLoad} />
+      </div>
+      <div className="message">
+        <h2>From {user.name}:</h2>
+        <p>{user.greetings}</p>
       </div>
     </div>
   );
